4bd55c7d36e7e8007c6be93fe0ae3a8da246a53a
mariamihai <maria.ic.mihai@gmail.com>
2021-10-17 01:16:29 +0200

    [Episode 2] Updated the stack with new S3 bucket for resized images, added a new Lambda layer for rekFn, added new service Lambda function

--- lib/aws-dev-hour-building-modern-applications-stack.ts
+++ lib/aws-dev-hour-building-modern-applications-stack.ts
@@ -8,6 +8,7 @@ import iam = require('@aws-cdk/aws-iam');
 import { Duration } from '@aws-cdk/core';
 
 const imageBucketName = "cdk-rekn-bucket";
+const resizeImageBucketName = imageBucketName + "-resized";
 
 export class AwsDevHourBuildingModernApplicationsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -18,10 +19,23 @@ export class AwsDevHourBuildingModernApplicationsStack extends cdk.Stack {
     // Image Bucket
     // --------------------------------------------
     // Create bucket
-    const imageBucket = new s3.Bucket(this, imageBucketName);
+    const imageBucket = new s3.Bucket(this, imageBucketName, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
     // Give some output after the cdk has been deployed and the resource has been created
     // (CloudFormation output)
     new cdk.CfnOutput(this, 'imageBucket', {value: imageBucket.bucketName});
+
+
+    // --------------------------------------------
+    // Thumbnail Bucket
+    // --------------------------------------------
+    const resizeImageBucket = new s3.Bucket(this, resizeImageBucketName, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+    new cdk.CfnOutput(this, 'resizeImageBucket', {value: resizeImageBucket.bucketName});
 
 
     // --------------------------------------------
@@ -31,13 +45,25 @@ export class AwsDevHourBuildingModernApplicationsStack extends cdk.Stack {
       partitionKey: {
         name: 'image',
         type: dynamodb.AttributeType.STRING
-      }
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
     })
     new cdk.CfnOutput(this, 'dbTable', {value: table.tableName});
 
 
     // --------------------------------------------
-    // Lambda Function
+    // Lambda Layer
+    // --------------------------------------------
+    const pilLayer = new lambda.LayerVersion(this, 'pil', {
+      code: lambda.Code.fromAsset('reklayer'),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_7],
+      license: 'Apache-2.0',
+      description: 'A layer to enable the PIL library in the Rekognition Lambda'
+    });
+
+
+    // --------------------------------------------
+    // Lambda Function for Rekognition
     // --------------------------------------------
     const rekFn = new lambda.Function(this, 'rekognitionFunction', {
       code: lambda.Code.fromAsset('rekognitionlambda'), // zip archive for the code
@@ -45,14 +71,17 @@ export class AwsDevHourBuildingModernApplicationsStack extends cdk.Stack {
       handler: 'index.handler',
       timeout: Duration.seconds(30),
       memorySize: 1024,
+      layers: [pilLayer],
       environment: {
         "TABLE": table.tableName,
-        "BUCKET": imageBucket.bucketName
-      },
+        "BUCKET": imageBucket.bucketName,
+        "RESIZEDBUCKET": resizeImageBucket.bucketName
+      }
     })
 
     rekFn.addEventSource(new event_sources.S3EventSource(imageBucket, { events: [ s3.EventType.OBJECT_CREATED ]}));
     imageBucket.grantRead(rekFn);
+    resizeImageBucket.grantPut(rekFn);
     table.grantWriteData(rekFn);
 
     rekFn.addToRolePolicy(new iam.PolicyStatement({
@@ -60,5 +89,24 @@ export class AwsDevHourBuildingModernApplicationsStack extends cdk.Stack {
       actions: ['rekognition:DetectLabels'],
       resources: ['*']
     }));
+
+
+    // --------------------------------------------
+    // Service Lambda Function
+    // --------------------------------------------
+    const serviceFn = new lambda.Function(this, 'serviceFunction', {
+      code: lambda.Code.fromAsset('servicelambda'),
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      environment: {
+        "TABLE": table.tableName,
+        "BUCKET": imageBucket.bucketName,
+        "RESIZEDBUCKET": resizeImageBucket.bucketName
+      },
+    });
+
+    imageBucket.grantWrite(serviceFn);
+    resizeImageBucket.grantWrite(serviceFn);
+    table.grantReadWriteData(serviceFn);
   }
 }
